bb475fa77b07fcfa31181fd7976bca925b8fec10
S-Github-26 <sukham_kaur@outlook.com>
2024-07-16 10:56:16 -0400

    users table with ngRx

---
+++ src/app/store/user.actions.ts
@@ -0,0 +1,25 @@
+import { createAction, props } from '@ngrx/store';
+import { User } from '../models/user.model';
+
+export const loadUsers = createAction('[User] Load Users');
+export const loadUsersSuccess = createAction(
+  '[User] Load Users Success',
+  props<{ users: User[] }>()
+);
+export const loadUsersFailure = createAction(
+  '[User] Load Users Failure',
+  props<{ error: any }>()
+);
+
+export const updateUser = createAction(
+  '[User] Update User',
+  props<{ user: User }>()
+);
+export const updateUserSuccess = createAction(
+  '[User] Update User Success',
+  props<{ user: User }>()
+);
+export const updateUserFailure = createAction(
+  '[User] Update User Failure',
+  props<{ error: any }>()
+);
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { userReducer } from './app/store/user.reducers';
+import { UserEffects } from './app/store/user.effects';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {providers: [
+  provideHttpClient(),
+  provideStore({ users: userReducer }),
+    provideEffects([UserEffects]),
+    provideStoreDevtools({ maxAge: 25 })
+]});

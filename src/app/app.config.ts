318a82b78091d59ec6f76c5bd1cac3bbeb6fe529
Marcus Ho <42228958+marcusho21@users.noreply.github.com>
2024-08-02 18:48:55 -0400

    initial setup for standalone and cleanup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideHttpClient(withFetch()), provideStore(), provideEffects()],
 };
---
+++ src/environment/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  api: 'https://jsonplaceholder.typicode.com/users',
+};

c87aac116178ea10ed623b72a0ba4488f6baf705
Marcus Ho <42228958+marcusho21@users.noreply.github.com>
2024-08-03 16:15:28 -0400

    1.3 Create a service to fetch the user list from the API (GET /users).

---
+++ src/app/users/data/users.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environment/environment';
+import { GetUserResponse } from '../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  private readonly ENDPOINT = `${environment.api}/users`;
+
+  private http = inject(HttpClient);
+
+  getAllUsers() {
+    return this.http.get<GetUserResponse>(this.ENDPOINT);
+  }
+}
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  api: 'https://jsonplaceholder.typicode.com/users',
+  api: 'https://jsonplaceholder.typicode.com',
 };
